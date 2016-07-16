38b375d0ce0f8175d2bd6bb890d56a082ccb5666
Anthony <martab7.am@gmail.com>
2016-07-16 16:29:31 -0500

    Updates index to include all newly updated files and brings in YouTube api

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import SearchBar from './components/search_bar'
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDRBuOWUytoJwNhFpD2DJiDBr6dZFG-Rvk';
 
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      <VideoDetail video={this.state.videos[0]} />
+      <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
